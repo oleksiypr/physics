30d2be20981063a72628a80abb1815fd43bcd9da
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-01-26 21:47:57 +0100

    problem 14.13 psi and C solved

--- feinman_14_13.py
+++ feinman_14_13.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import symbols, pretty_print
+from sympy import symbols, pretty_print, Function, Eq, integrate, simplify, solve
 from sympy.interactive import init_printing
 
 init_printing()
@@ -20,18 +20,43 @@ R = symbols('R')
 M = symbols('M')
 
 
-def psi(r):
-    """
-    Gravity potential
-    :param r: distance from sphere center, r <= R
-    """
-    return 1/2 * G * M/R**3 * (r**2 - 3*R**2)
+# Approach 1. Gravity potential.
+'''
+Divide ball into 2 pieces: 
+- inner: [0, r)
+- outer: [r, R]
+'''
 
+psi_1 = Function('psi_1')(r)
+psi_2 = Function('psi_2')(r)
+psi = Function('psi')(r)
 
-# check psi(R) = - G * M/R
-assert ((psi(r).subs(r, R) + G*M/R) == 0)
+eqP = Eq(psi, psi_1 + psi_2)
+pretty_print(eqP)
 
-# Gravity field
-C = - psi(r).diff(r)
+m = M * r**3 /R**3
+eqP = eqP.subs(psi_1, - G * m / r)
+pretty_print(eqP)
 
-pretty_print(C)
+r1 = symbols('r1')
+der_m = m.diff(r).subs(r, r1)
+p2 = integrate( - G * der_m/r1, (r1, r, R))
+p2 = simplify(p2)
+
+eqP = eqP.subs(psi_2, p2)
+pretty_print(simplify(eqP))
+
+psi = solve(eqP, psi)[0]
+
+# assert edge case for r = R
+assert psi.subs(r, R) + G * M/R == 0
+
+C = Function('C')(r)
+eqC = Eq(C, - psi.diff(r))
+pretty_print(eqC)
+
+C = solve(eqC, C)[0]
+
+# assert edge case for r = R
+assert C.subs(r, R) + G * M/R**2 == 0
+
