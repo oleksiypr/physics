a09f4722f69ed210ac1ee1ba12a562f66c2f9ad1
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-01-27 21:32:47 +0100

    feinman 14.13: approach 2: gravity field applied

--- feinman/feinman_14_13.py
+++ feinman/feinman_14_13.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import symbols, pretty_print, Function, Eq, integrate, simplify, solve
+from sympy import symbols, pretty_print, Function, Eq, integrate, simplify, solve, Integral, Derivative, dsolve
 from sympy.interactive import init_printing
 
 init_printing()
@@ -11,12 +11,13 @@ R = symbols('R')
 M = symbols('M')
 
 
-# Approach 1. Gravity potential.
-'''
+pretty_print('''Approach 1. Gravity potential''')
+
+pretty_print('''
 Divide ball into 2 pieces: 
-- inner: [0, r)
-- outer: [r, R]
-'''
+1: inner: [0, r)
+2: outer: [r, R]
+According to superposition principle gravity potential:\n\n''')
 
 psi_1 = Function('psi_1')(r)
 psi_2 = Function('psi_2')(r)
@@ -25,24 +26,33 @@ psi = Function('psi')(r)
 eqP = Eq(psi, psi_1 + psi_2)
 pretty_print(eqP)
 
+pretty_print('''\nFor inner ball mass is:''')
+
 m = M * r**3 /R**3
+pretty_print(m)
+
+pretty_print('\nThen:')
 eqP = eqP.subs(psi_1, - G * m / r)
 pretty_print(eqP)
 
+pretty_print('''\n For outer:''')
 r1 = symbols('r1')
 der_m = m.diff(r).subs(r, r1)
+pretty_print(Eq(psi_2, Integral( - G * der_m/r1, (r1, r, R))))
 p2 = integrate( - G * der_m/r1, (r1, r, R))
 p2 = simplify(p2)
 
 eqP = eqP.subs(psi_2, p2)
 pretty_print(simplify(eqP))
 
+pretty_print('''Then: ''')
 psi = solve(eqP, psi)[0]
 
 # assert edge case for r = R
 assert psi.subs(r, R) + G * M/R == 0
 
 C = Function('C')(r)
+pretty_print(Eq(C, -  Derivative(Function('psi')(r), r)))
 eqC = Eq(C, - psi.diff(r))
 pretty_print(eqC)
 
@@ -76,3 +86,23 @@ rs = np.linspace(1., 4.)
 plt.plot(rs, 1 / rs ** 2)
 
 plt.show()
+
+pretty_print('''Approach 2. Gravity filed''')
+
+psi = Function('psi')(r)
+C = Function('C')(r)
+pretty_print(Eq(C, -  Derivative(Function('psi')(r), r)))
+
+diff_eq = Eq(psi.diff(r), G*M/R**3 * r)
+pretty_print(diff_eq)
+
+solution = dsolve(diff_eq, psi)
+pretty_print(solution)
+
+C1 = solution.subs({
+    psi: -G*M/R,
+    r: R
+})
+
+C1 = solve(C1, symbols('C1'))[0]
+pretty_print(simplify(solution.subs('C1', C1)))
