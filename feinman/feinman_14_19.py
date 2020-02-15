c32663f9cd632bcaa8cdb6b40fb5bf0fa25c1dbc
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-02-15 16:09:53 +0100

    feinman 14.19 (b): solved

--- feinman/feinman_14_19.py
+++ feinman/feinman_14_19.py
@@ -3,10 +3,11 @@ import sympy as sym
 import matplotlib.pyplot as plt
 from sympy import symbols, pretty_print, Function, Eq, integrate, simplify, solve, Integral, Derivative, dsolve
 from sympy.interactive import init_printing
+from sympy.functions import *
 
 init_printing()
 
-# a)
+print('a).')
 
 a = symbols('a')
 b = symbols('b')
@@ -20,3 +21,43 @@ k = symbols('k')
 S = k * S_o
 S = S.subs(k, a/b)
 pretty_print(S)
+
+print('\n')
+print('b).')
+
+T = symbols('T')
+S = S.subs(b, a * sqrt(1 - e**2))
+pretty_print(S)
+
+# let sigma = dS/dt
+# for ellipse sigma = const by Kepler 2nd law
+sigma = symbols('sigma')
+
+# then:
+eq_T = Eq(T, S / sigma)
+pretty_print(eq_T)
+
+# let v - speed in point B, where point B is a point where
+# low axis crossing ellipse:
+v = symbols('v')
+b = a * sqrt(1 - e**2)
+
+# then for point B:
+sigma_eq_B = Eq(sigma, 1/2 * v * b)
+pretty_print(sigma_eq_B)
+
+# from  feinman_14_18:
+G, M, m = symbols('G, M, m')
+E = -1/2 * G * M*m / a
+eq_E = Eq(m * v**2 / 2 - G*M*m/a, E)
+pretty_print(eq_E)
+
+# then:
+
+T, _, _ = solve([
+    sigma_eq_B,
+    eq_E,
+    eq_T
+], T, sigma, v)[1]
+
+pretty_print(T**2)
