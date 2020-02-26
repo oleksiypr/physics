d3f8891c4f8fc994e325483129cecd71b54632c7
Oleksii Prosianko <oleksii.prosianko@fedex.com>
2020-02-26 14:21:36 +0100

    Moved cache research to separate project

--- feinman/functions_plot_trial.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-lambda_h = 1.0
-lambda_r = 1.5
-
-t = np.linspace(0., 2.)
-#H = lambda_h *
--- feinman/utils.py
+++ feinman/utils.py
@@ -2,5 +2,12 @@ from sympy.physics.units.systems import SI
 
 
 def set_quantity(unit, dimension, scale_factor):
+    """
+    set value and unit to physical quantity
+    :param unit:
+    :param dimension:
+    :param scale_factor:
+    :return:
+    """
     SI.set_quantity_dimension(unit, dimension)
     SI.set_quantity_scale_factor(unit, scale_factor)
