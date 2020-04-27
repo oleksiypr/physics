77c27324344b42f5369469275a1d55ba870d2e01
oleksiyprosyanko <oleksii.prosianko@fedex.com>
2020-04-27 18:46:11 +0200

    feinman 15.03 (b): solved

--- feinman/feinman_15_04.py
+++ feinman/feinman_15_04.py
@@ -24,11 +24,42 @@ pretty_print(Eq(symbols('D'), symbols('H_^2')))
 pretty_print(Eq(symbols('H^2') + symbols('H^2'), symbols('He_^4')))
 
 print('a).')
-
+m = symbols('m')
+pretty_print(Eq(m, symbols('E')/c**2))
 m = E/c**2
 m = convert_to(m, un.kg).n()
 
 pretty_print("Energy to mass  : {}".format(m))
+
+print('b).')
+M_D   = Quantity('M_D')
+M_He4 = Quantity('M_He4')
+M_O   = Quantity('M_O')
+
+set_quantity(M_D,   un.mass/un.amount_of_substance,  2.0147 * un.gram / un.mol)
+set_quantity(M_He4, un.mass/un.amount_of_substance,  4.0039 * un.gram / un.mol)
+set_quantity(M_O,   un.mass/un.amount_of_substance, 15.9990 * un.gram / un.mol)
+
+dm = 2*M_D - M_He4
+dm = convert_to(dm, un.gram / un.mol).n()
+print('dm = {}'.format(dm))
+
+v_He = m/dm
+v_He = convert_to(v_He, un.mol).n()
+
+v_D2O = v_He
+print('v_D2O = {}'.format(v_D2O))
+M_D20 = 2*M_D + M_O
+M_D20 = convert_to(M_D20, un.gram / un.mol)
+print('M_D20 = {}'.format(M_D20))
+
+m_D20 = M_D20 * v_D2O
+m_D20 = convert_to(m_D20, un.kg)
+print('Mass of heavy water for nucleosynthesis reaction for a year: {}'.format(m_D20))
+
+m_D20_per_second = m_D20/365/24/3600
+m_D20_per_second = convert_to(m_D20_per_second, un.grams)
+print('Mass of heavy water for nucleosynthesis reaction per second: {}'.format(m_D20_per_second))
 
 
 
